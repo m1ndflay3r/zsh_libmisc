62873d41ac970f861061420ae4c52f22eca83bc2
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2023-07-30 00:16:32 -0600

    continue work on template

--- zsh/working/template.zsh
+++ zsh/working/template.zsh
@@ -16,6 +16,7 @@ template() {
     template_arg_array+=("$template_args")
   done
   #stdin read
+  ztemplate_stdin_empty=0
   if [ -z "$ztemplateWAITINDEF" ] || [ "$ztemplateWAITINDEF" = 0 ]; then
     export zFOXT=3
   else
@@ -30,11 +31,12 @@ template() {
     if [ -z "$@" ]; then
       template_fail
       return 1
-#    else
+    else
 #      ztemplate_stdin="$@"                      #uncomment to treat input as file path if file exists at that path
 #      if isfile "$ztemplate_stdin"; then
 #        ztemplate_stdin="$(fox-fr "$ztemplate_stdin")"
 #      fi
+      ztemplate_stdin_empty=1
     fi
     # allow for manual file readin through filepath on stdin (print -n "file:/home/user/file.txt" | template)
   elif [ "$ztemplate_stdin_read_return" = 'file:' ]; then
@@ -46,6 +48,11 @@ template() {
       return 1
     fi
   fi
+  if [ "$ztemplate_stdin_empty" != 1 ]; then
+    export zSTDIN=$ztemplate_stdin
+  else
+    unset zSTDIN
+  fi
   ## env setup
   if [ ! -d "/tmp" ]; then
     template_working_dir=""$HOME"/template"
@@ -56,4 +63,12 @@ template() {
   [ ! -d "$template_working_dir" ] && zmkdir -p "$template_working_dir"
   #set process name for template binary
   export zELFSPOOF="template"
+  ## define template binary as function
+  ztemplate_binary() {
+    #insert hexdump created with "zHEXDSH=1 hexd /path/to/file"
+    print -n ""
+  }
+  ## execution
+  (ztemplate_binary | zSTDIN=$zSTDIN zelfexec $=template_arg_array)
+  return $?
 }
