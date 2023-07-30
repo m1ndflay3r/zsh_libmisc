d628e64099ba7ccab0a83a32e8d0dc514025e351
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2023-07-29 23:39:40 -0600

    continue work on template

--- zsh/working/template.zsh
+++ zsh/working/template.zsh
@@ -1,13 +1,21 @@
 #!/usr/bin/env zsh
 
 template() {
-  #load modules
+  ## load modules
   autoload libmiscinit           #
   libmiscinit                    # this loads literally every libmisc module. Replace with needed functions via autoload
+  ## failure printout
   template_fail() {
     print 'Usage: <usage information>'
   }
-  #fetch user input, trying stdin first
+  ## fetch user input
+  #read command line args to array
+  unset template_arg_array
+  declare -a template_arg_array
+  for template_args in $=@; do
+    template_arg_array+=("$template_args")
+  done
+  #stdin read
   if [ -z "$ztemplateWAITINDEF" ] || [ "$ztemplateWAITINDEF" = 0 ]; then
     export zFOXT=3
   else
@@ -16,18 +24,20 @@ template() {
   fi
   ztemplate_stdin="$(fox)"
   unset zFOXT
-  ztemplTESTWX="${ztemplate_stdin: :5}"
-  if [ "$ztemplTESTWX" = "error" ]; then
+  ztemplate_stdin_read_return="${ztemplate_stdin: :5}"
+  if [ "$ztemplate_stdin_read_return" = "error" ]; then
+    # error out if both stdin and command line args are empty
     if [ -z "$@" ]; then
       template_fail
       return 1
-    else
-      ztemplate_stdin="$@"
-      if isfile "$ztemplate_stdin"; then
-        ztemplate_stdin="$(fox-fr "$ztemplate_stdin")"
-      fi
+#    else
+#      ztemplate_stdin="$@"                      #uncomment to treat input as file path if file exists at that path
+#      if isfile "$ztemplate_stdin"; then
+#        ztemplate_stdin="$(fox-fr "$ztemplate_stdin")"
+#      fi
     fi
-  elif [ "$ztemplTESTWX" = 'file:' ]; then
+    # allow for manual file readin through filepath on stdin (print -n "file:/home/user/file.txt" | template)
+  elif [ "$ztemplate_stdin_read_return" = 'file:' ]; then
     ztemplate_stdin="${ztemplate_stdin:5}"
     if isfile "$ztemplate_stdin"; then
       ztemplate_stdin="$(fox-fr "$ztemplate_stdin")"
@@ -36,12 +46,14 @@ template() {
       return 1
     fi
   fi
-  #env setup
+  ## env setup
   if [ ! -d "/tmp" ]; then
-    template_working_dir=$HOME
+    template_working_dir=""$HOME"/template"
   else
-    template_working_dir='/tmp'
+    template_working_dir='/tmp/template'
   fi
-  #set process name for template binary to template
+  #create working directory if not exist
+  [ ! -d "$template_working_dir" ] && zmkdir -p "$template_working_dir"
+  #set process name for template binary
   export zELFSPOOF="template"
 }
